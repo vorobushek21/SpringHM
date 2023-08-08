e931092a8bc53599baee0e9d8361a7495b114313
vorobushek <anton.vorobev1999@mail.ru>
2023-08-08 21:40:03 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/lessons/springboot/springhm/SpringHmApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.springhm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/lessons/springboot/springhm/SpringHmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.springhm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
